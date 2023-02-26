b599b91d17305995f041ba4ee20942cffb145bd4
baklyNik <baklykov.nikita369@gmail.com>
2023-02-26 23:47:36 +0300

    init

---
+++ src/main/java/com/example/telldate/TellDateApplication.java
@@ -0,0 +1,13 @@
+package com.example.telldate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellDateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TellDateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telldate/TellDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telldate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellDateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
